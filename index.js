d71bec2b723b100701b923bc6ffd505cf0e27f55
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-03-26 22:31:13 -0400

    5.2.5.3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import Hello from "./hello.js";
+// import Hello from "./hello.js";
 import Lab5 from "./lab5/index.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 Lab5(app);
-Hello(app);
+// Hello(app);
 
 app.listen(process.env.PORT || 4000)
 
