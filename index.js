0ec66d1085997d56e3420321f68611426ba124a0
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-03-31 23:24:47 -0400

    5.2.6.1

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import Lab5 from "./lab5/index.js";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
 Lab5(app);
 // Hello(app);
 
--- lab5/working_with_arrays.js
+++ lab5/working_with_arrays.js
@@ -24,6 +24,12 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
 
     app.get("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
