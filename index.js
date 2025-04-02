f483e9c2dd323814002f3a0de9ed4950a5e56b9c
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-04-01 23:34:02 -0400

    5.3.2.1

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-import express from 'express';
-// import Hello from "./hello.js";
-import Lab5 from "./lab5/index.js";
+// import "dotenv/config";
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import UserRoutes from "./kambaz/users/routes.js"
 import cors from "cors";
+import session from "express-session";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 
+// app.use(
+//     cors({
+//         credentials: true,
+//         origin: process.env.NETLIFY_URL || "http://localhost:5173",
+//     })
+// );
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+Hello(app);
 Lab5(app);
-// Hello(app);
+app.listen(4000);
 
-app.listen(process.env.PORT || 4000)
-
+// app.listen(process.env.PORT || 4000);
