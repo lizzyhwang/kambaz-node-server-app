0bb7efb78a5d0c0d2b5c1858b8ff0c39bf5fdcc2
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-04-16 13:11:02 -0400

    6.4.3.4

--- kambaz/courses/routes.js
+++ kambaz/courses/routes.js
@@ -1,6 +1,7 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../modules/dao.js";
 import * as assignmentsDao from "../assignments/dao.js"
+import * as enrollmentsDao from "../enrollments/dao.js";
 
 export default function CourseRoutes(app) {
     app.get("/api/courses", async (req, res) => {
@@ -10,6 +11,10 @@ export default function CourseRoutes(app) {
 
     app.post("/api/courses", async (req, res) => {
         const course = await dao.createCourse(req.body);
+        const currentUser = req.session["currentUser"];
+        if (currentUser) {
+            await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+        }
         res.json(course);
     });
 
