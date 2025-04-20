5118138c1114042e70d3eca3dd60d82cb12b8655
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-04-20 00:54:40 -0400

    unenroll all users from course if deleting the course

--- kambaz/courses/routes.js
+++ kambaz/courses/routes.js
@@ -29,6 +29,7 @@ export default function CourseRoutes(app) {
     app.delete("/api/courses/:courseId", async (req, res) => {
         const { courseId } = req.params;
         const status = await dao.deleteCourse(courseId);
+        await enrollmentsDao.unenrollAllUsersFromCourse(courseId);
         res.send(status);
     });
 
